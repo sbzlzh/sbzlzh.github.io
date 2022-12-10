fa7582cdde9a9db21516a4cb8ca79f1fad7681bd
波と一緒に漂う <2247564152@qq.com>
2022-12-10 15:16:49 +0800

    Add files via upload

---
+++ languages/chinese.js
@@ -0,0 +1,3 @@
+language.below_logo = "服务器正在运行游戏模式:%%gamemode%%|地图:%%map%%"
+language.loading = "正在加载插件中..."
+language.now_playing = "正在播放音乐:"
---
+++ languages/english.js
@@ -0,0 +1,3 @@
+language.below_logo = "服务器正在运行模式 %%gamemode%% at %%map%%"
+language.loading = "正在加载插件中..."
+language.now_playing = "正在播放"
