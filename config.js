ee62c0f67f3490df11d4c980a09f6981691fe0ac
波と一緒に漂う <2247564152@qq.com>
2022-12-10 15:25:15 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,173 @@
+//  ____  _           _    _                    _
+// / ___|| | ___  ___| | _| |    ___   __ _  __| |
+// \___ \| |/ _ \/ _ \ |/ / |   / _ \ / _` |/ _` |
+//  ___) | |  __/  __/   <| |__| (_) | (_| | (_| |
+// |____/|_|\___|\___|_|\_\_____\___/ \__,_|\__,_|
+// version 2.17.1
+
+// Loading screen language
+//
+// You can create or edit languages by
+// editing/creating files in the languages folder
+//
+// Default available languages:
+// "english"
+// "brazilian_portuguese"
+config.language = "chinese"
+
+// Use an image for the logo instead of text
+config.logo_use_image = false
+
+// Image filename for the server logo
+// (Place the image file in the images folder)
+config.logo_image = ""
+
+// Text to be shown on logo
+config.logo_text = ""
+
+// Use the server's name for the logo
+config.logo_servername = true
+
+// Center logo?
+// "true" or "false"
+config.logo_center = true
+
+// Display current map and gamemode?
+// "true" or "false"
+config.logo_show_info = true
+
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the image files in the backgrounds/images folder)
+config.background_images = [
+	"1.jpg",
+	"z.jpg",
+	"x.jpg",
+	"2.jpg",
+	"3.jpg",
+	"q.jpg",
+	"4.jpg",
+	"5.jpg",
+	"6.jpg",
+	"7.jpg",
+	"8.jpg",
+	"9.jpg",
+	"10.jpg",
+	"11.jpg",
+	"12.jpg",
+	"v.jpg",
+	"n.jpg",
+	"m.jpg",
+	"e.jpg",
+	"r.jpg",
+	"13.jpg",
+	"14.jpg",
+	"t.jpg",
+	"15.jpg",
+	"16.jpg",
+	"17.jpg",
+	"18.jpg",
+	"19.jpg",
+	"20.jpg",
+	"21.jpg",
+	"22.jpg",
+	"23.jpg",
+	"24.jpg",
+	"43.jpg",
+	"44.jpg",
+	"45.jpg",
+	"42.jpg",
+	"41.jpg",
+	"26.jpg",
+	"y.jpg",
+	"27.jpg",
+	"28.jpg",
+    "29.jpg",
+    "30.jpg",
+    "31.jpg",
+    "32.jpg",
+    "33.jpg",
+    "34.jpg",
+    "o.jpg",
+    "35.jpg",
+    "ps.jpg",
+    "f.jpg",
+    "g.jpg",
+    "36.jpg",
+    "37.jpg",
+    "38.jpg",
+    "pic.jpg",
+    "39.jpg",
+    "40.jpg",
+    "h.jpg",
+    "j.jpg",
+	"w.jpg",
+	"s.jpg",
+	"b.jpg",
+	"c.jpg",
+	"d.jpg",
+	"p.jpg",
+	"q.jpg",
+	"u.jpg",
+]
+
+// (Image-background only)
+// Random background images order?
+// "true" or "false"
+config.background_images_random_order = true
+
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+config.background_images_duration = 6000
+
+// (Image-background only)
+// Image background fade duration
+// in milliseconds
+config.background_images_fade_duration = 4000
+
+// Enable background overlay?
+// "true" or "false"
+config.background_overlay = true
+
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+config.background_darkening = 45
+
+// Play music during the loading screen?
+// "true" or "false"
+config.music_enable = false
+
+// Display current music name?
+// "true" or "false"
+config.music_show = false
+
+// Music playlist
+// Add as many youtube ids/urls or audio files you want
+// (YouTube only works on Chromium beta)
+// (Use ogg files for compatibility with default Gmod)
+// (Place the audio files in the music folder)
+
+//config.music_playlist = [
+//	{
+//		name: "",
+//		file: "",
+//	},
+//]
+
+// Random music order?
+// "true" or "false"
+config.music_random_order = false
+
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+config.music_volume = 25
+
+// Use Gmod volume for the music instead of manual volume
+config.music_gmod_volume = false
+
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// "true" or "false"
+config.errors_show_ingame = true
